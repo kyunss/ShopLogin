cbbf25b7b5742cc44973388617d7c4816832acb3
Daniel.Kwak <ukhyun91@gmail.com>
2019-01-02 17:59:35 +0900

    DataBinding 초기 작업

--- app/src/main/java/com/example/daniel/shoplogin/LoginActivity.java
+++ app/src/main/java/com/example/daniel/shoplogin/LoginActivity.java
@@ -3,14 +3,27 @@ package com.example.daniel.shoplogin;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.daniel.shoplogin.databinding.ActivityLoginBinding;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private final String TAG = getClass().getName();
+    private ActivityLoginBinding mBinding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mBinding = DataBindingUtil.setContentView(this , R.layout.activity_login);
+        mBinding.setActivity(this);
+
+
+    }
+
+    public void onLoginButtonClicked(View view) {
+        Log.d(TAG , "LoginButtonClicked");
     }
 }
