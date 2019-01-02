492b9cef4e832ce05b789a3d0fb51d6835c4f6ca
Daniel.Kwak <ukhyun91@gmail.com>
2019-01-02 15:10:34 +0900

    build 에러 수정 in Windoe

--- app/src/main/java/com/example/daniel/shoplogin/LoginActivity.java
+++ app/src/main/java/com/example/daniel/shoplogin/LoginActivity.java
@@ -4,12 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class LoginActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.activity_login);
+        setContentView(R.layout.activity_login);
     }
 }
